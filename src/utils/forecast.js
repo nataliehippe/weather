2031d6a57c306b624822673bd343b3372ddcffba
nataliehippe <natalie.alibegashvili@gmail.com>
2021-05-14 15:54:26 +0400

    replace nodemons global module with local one

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,15 +3,15 @@ const request = require('postman-request');
 
 
 const forecast = (lat, long, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=95963a94a7f64d5c6bd0f4e460db30ea&query=' + lat + ',' + long+'&units=m';
+    const url = 'http://api.weatherstack.com/current?access_key=95963a94a7f64d5c6bd0f4e460db30ea&query=' + lat + ',' + long + '&units=m';
 
-    request({url, json: true }, (error, {body}) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service', undefined);
         } else if (body.length === 0) {
             callback('Coordinate error', undefined);
         } else {
-            callback(undefined, body.location.name + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
+            callback(undefined, body.location.name + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + "%.")
         }
     });
 
